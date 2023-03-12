d9eefc56033108b3fc0e65f138ebf2ad3312e7cd
SnowLober <80399342+SnowLober@users.noreply.github.com>
2023-03-12 13:57:12 +0000

    13/2

--- daluck/pages/comp/Topbar.tsx
+++ daluck/pages/comp/Topbar.tsx
@@ -1,6 +1,6 @@
 const Topbar = () => {
     return(
-        <div className="top-0 h-12 w-screen flex bg-green-300 shadow " >
+        <div className="top-0 h-12 w-screen flex bg-transparent shadow " >
             <a className="mx-3 my-auto">DaLuck</a>
             <br></br>
             <a className=""></a>
